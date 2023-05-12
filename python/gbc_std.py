3fb65a2e9c7ebe65929e0d538ebc626429924129
Lewis <126605481+green-box-code@users.noreply.github.com>
2023-05-12 15:48:50 -0600

    Create gbc_std.py
    Calculate the Standard Deviation of an array

---
+++ python/gbc_std.py
@@ -0,0 +1,26 @@
+import math # Python built in Library for square root function
+import warnings
+
+x= [3.2, 3.12, 3.3, 4.33, 4.67, 4.44]
+
+def sample_std(arr):
+    sum = 0
+    count = len(arr)
+    mu = my_mean(arr)
+    for i in arr:
+        #The Summation
+        sum = sum + (i - mu) ** 2
+    return (math.sqrt(sum/(count - 1)))
+
+def my_mean(arr):
+    sum = 0
+    count =  len(arr)
+    for i in arr:
+        sum+=i
+    result = sum / count
+    print(f'The mean is: {result}')
+    return result
+
+sstd = sample_std(x)
+
+print(f'The set of {x} has a sample standard deviation of {sstd}')
