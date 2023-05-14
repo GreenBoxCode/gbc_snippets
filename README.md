c8e206daaf1f3e87cdec66df88d78ba36b4e74e3
mlewis <mlewis@greenboxcode.com>
2023-05-14 06:59:01 -0600

    Added person script

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # gbc_snippets
+
 ## A collection of code snippets that might be handy
+
 ### Python
+
 - Great Circle Route Calculation using the Haversine Formula [python/gbc_gcr.py](https://github.com/GreenBoxCode/gbc_snippets/blob/main/python/gbc_gcr.py)
 - Calculate the square root of a positive real number [python/gbc_sqrt.py](https://github.com/GreenBoxCode/gbc_snippets/blob/main/python/gbc_sqrt.py)
-- Calculate the Standard Deviation of and array [python/gbc_std.py](https://github.com/GreenBoxCode/gbc_snippets/blob/main/python/gbc_std.py)
+- Calculate the Standard Deviation of an array [python/gbc_std.py](https://github.com/GreenBoxCode/gbc_snippets/blob/main/python/gbc_std.py)
+
 ### C / C++
 
 ### Others
--- python/gbc_gcr.py
+++ python/gbc_gcr.py
@@ -2,29 +2,31 @@ import math
 
 
 def get_distance_and_bearing(lat1, lon1, lat2, lon2):
-    # Convert decimal degrees to radians
-    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+    try:
+        # Convert decimal degrees to radians
+        lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
 
-    # Calculate the distance between the two points in Km
-    d = 6371.01 * math.acos(
-        math.sin(lat1) * math.sin(lat2)
-        + math.cos(lat1) * math.cos(lat2) * math.cos(lon2 - lon1)
-    )
+        # Calculate the distance between the two points in Km
+        d = 6371.01 * math.acos(
+            math.sin(lat1) * math.sin(lat2)
+            + math.cos(lat1) * math.cos(lat2) * math.cos(lon2 - lon1)
+        )
 
-    # Calculate the bearing (clockwise from north)
-    bearing = math.atan2(
-        math.sin(lon2 - lon1) * math.cos(lat2),
-        math.cos(lat1) * math.sin(lat2)
-        - math.sin(lat1) * math.cos(lat2) * math.cos(lon2 - lon1),
-    )
+        # Calculate the bearing (clockwise from north)
+        bearing = math.atan2(
+            math.sin(lon2 - lon1) * math.cos(lat2),
+            math.cos(lat1) * math.sin(lat2)
+            - math.sin(lat1) * math.cos(lat2) * math.cos(lon2 - lon1),
+        )
 
-    # Convert the bearing to degrees
-    print(f"Bearing in Radians {bearing}")
-    bearing = math.degrees(bearing)
-    if bearing < 0:
-        bearing += 360
-    # Return the distance and bearing
-    return d, bearing
+        # Convert the bearing to degrees
+        bearing = math.degrees(bearing)
+        if bearing < 0:
+            bearing += 360
+        # Return the distance and bearing
+        return d, bearing
+    except Exception as e:
+        print(f"An error has occured:/n{e}")
 
 
 # Example Vancouver to Honolulu
